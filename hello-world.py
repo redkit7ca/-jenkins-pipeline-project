6876dd146b60a8f47eb282d8bd47d41d6bc8157a
redkit7ca <60303725+redkit7ca@users.noreply.github.com>
2021-10-01 15:36:17 -0400

    configuration changed

--- hello-world.py
+++ hello-world.py
@@ -3,3 +3,4 @@
 "forgot to add /github-webhook/ part then added"
 "jenkins plugin added this time"
 "test the webhook again after restart Jenkins server"
+"git link added Source Code MAnagement Tab instead of General Tab in Jenkins new item menu"
