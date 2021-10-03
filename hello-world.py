14afccee93708aeadb7faa61cf24b717f5e1d4b6
redkit7ca <60303725+redkit7ca@users.noreply.github.com>
2021-10-03 16:55:16 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,6 @@
-"Hello Jenkins Learner!"
+print("Hello Jenkins Learner!"
 "Added a webhook this time!"
 "forgot to add /github-webhook/ part then added"
 "jenkins plugin added this time"
 "test the webhook again after restart Jenkins server"
-"git link added Source Code MAnagement Tab instead of General Tab in Jenkins new item menu" "Yay !! it worked!"
+"git link added Source Code MAnagement Tab instead of General Tab in Jenkins new item menu" "Yay !! it worked!")
