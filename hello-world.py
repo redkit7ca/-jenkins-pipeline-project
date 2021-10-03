62920543e1a7f238112fbed25931922cf97b15dd
redkit7ca <60303725+redkit7ca@users.noreply.github.com>
2021-10-03 16:58:35 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,1 @@
-print("Hello Jenkins Learner!"
-"Added a webhook this time!"
-"forgot to add /github-webhook/ part then added"
-"jenkins plugin added this time"
-"test the webhook again after restart Jenkins server"
-"git link added Source Code MAnagement Tab instead of General Tab in Jenkins new item menu" "Yay !! it worked!", "it was not a python code before, thats way was not working.")
+print("it was not a python code before, thats way was not working.")
